91e163c6916a2eabed1d2f222878085e751e495f
Brooke Kuhlmann <brooke@redalchemist.com>
2011-08-27 14:09:24 -0600

    Removed the unused namespace placeholder.

--- lib/heroku_plus.rb
+++ lib/heroku_plus.rb
@@ -2,7 +2,3 @@ require File.join File.dirname(__FILE__), "heroku_plus", "version.rb"
 require File.join File.dirname(__FILE__), "heroku_plus", "utilities.rb"
 require File.join File.dirname(__FILE__), "heroku_plus", "credentials.rb"
 require File.join File.dirname(__FILE__), "heroku_plus", "identity.rb"
-
-module HerokuPlus
-  # Placeholder.
-end
--- lib/heroku_plus/version.rb
+++ lib/heroku_plus/version.rb
@@ -1,3 +1,3 @@
 module HerokuPlus
-  VERSION = "3.3.0"
+  VERSION = "3.3.1"
 end
